832225107a8073ffa8e009650cc0ec6b6e05b03f
Andrew Cochran <andrew@dynamicpdf.com>
2019-08-23 10:59:46 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using ceTe.DynamicPDF.Rasterizer;
+using System;
+using System.Text.RegularExpressions;
+
+namespace example_pdf_bmp_dotnet
+{
+    // This example shows how to convert a PDF document to BMP.
+    // It references the ceTe.DynamicPDF.Rasterizer.NET NuGet package.
+    class Program
+    {
+        // Convert a PDF document to 4 PNG files (one for each page).
+        // Use the ceTe.DynamicPDF.Rasterizer namespace for the PdfRasterizer class.
+        static void Main(string[] args)
+        {
+            // Create a PdfRasterizer object using the source PDF to be converted to BMP images
+            PdfRasterizer rasterizer = new PdfRasterizer(GetResourcePath("doc-a.pdf"));
+
+            // Call the Draw method with output image name, image format and the DPI
+            rasterizer.Draw("output.bmp", ImageFormat.Bmp, ImageSize.Dpi96);
+        }
+
+        // This is a helper function to get the full path to a file in the Resources folder.
+        public static string GetResourcePath(string inputFileName)
+        {
+            var exePath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
+            Regex appPathMatcher = new Regex(@"(?<!fil)[A-Za-z]:\\+[\S\s]*?(?=\\+bin)");
+            var appRoot = appPathMatcher.Match(exePath).Value;
+            return System.IO.Path.Combine(appRoot, "Resources", inputFileName);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+﻿# Convert PDF to BMP Example for C# (.NET Core)
+
+This example shows how to convert a PDF document to BMP images. More details can be found
+[here](https://www.dynamicpdf.com/examples/pdf-to-bmp-.net-core).
